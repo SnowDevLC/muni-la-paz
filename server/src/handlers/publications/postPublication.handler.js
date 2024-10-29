17f09fa1c82e611d91e4836d1d620117621c7187
Luis Canales <canalesluis9@gmail.com>
2024-10-29 11:24:17 -0300

    add console.log en publication

--- server/src/handlers/publications/postPublication.handler.js
+++ server/src/handlers/publications/postPublication.handler.js
@@ -1,10 +1,12 @@
 const { postPublicationController } = require('../../controllers');
 
 module.exports = async (req, res) => {
+  console.log("ENTRO AL HANDLER")
   try {
     const postPublication = await postPublicationController(req.body, req.files, req.user.id);
     res.status(200).json(postPublication);
   } catch (error) {
+    console.log("ERROR:", error);
     res.status(400).json({ error: error.message });
   }
 };
